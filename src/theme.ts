8a2828a80155c23cebefb6de98e666bd6e6623c6
Canh Duong <duominhcanh@gmail.com>
2025-03-19 14:48:10 +0700

    Add dark color palette to theme configuration

--- src/theme.ts
+++ src/theme.ts
@@ -20,6 +20,20 @@ export const theme = createTheme({
   fontSmoothing: true,
   primaryColor: "cyan",
   defaultRadius: "md",
+  colors: {
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5c5f66",
+      "#373A40",
+      "#2C2E33",
+      "#25262b",
+      "#1A1B1E",
+      "#141517",
+      "#101113",
+    ],
+  },
   components: {
     Button: {
       defaultProps: {
